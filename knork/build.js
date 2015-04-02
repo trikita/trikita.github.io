bd5c1c4a91f98e89b522ac94f8f9e6a515c5461d
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-04-02 21:16:13 +0300

    fixed knork docs

---
+++ knork/build.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var less = require('less');
+var marked = require('marked');
+
+var renderer = new marked.Renderer();
+
+renderer.heading = function (text, level) {
+	var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+	return '<h' + level + ' id="markdown-header-'+escapedText+'">' +
+		text + '</h' + level + '>';
+};
+
+var template = fs.readFileSync(process.argv[3], 'utf8');
+var content = fs.readFileSync(process.argv[2], 'utf8');
+var css = fs.readFileSync('styles.less', 'utf8');
+less.render(css, function(e, output) {
+	if (e) {
+		console.log(e);
+		return;
+	}
+	fs.writeFileSync('styles.css', output.css);
+});
+
+var html = template.replace('$CONTENT', marked(content, {
+	renderer: renderer,
+	gfm: true
+}));
+fs.writeFileSync('index.html', html);
+
