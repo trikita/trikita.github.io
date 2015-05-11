1a9b99bdb2f6baa7cc561f4b1e595d0cc7758d74
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-05-12 01:46:27 +0300

    fixed titles in knork and chain

--- knork/app.js
+++ knork/app.js
@@ -44,7 +44,7 @@ window.onload = function() {
 	footer.parentNode.insertBefore(disqus, footer);
 
 	// Disqus
-	var disqus_shortname = 'trikita-site';
+	var disqus_shortname = 'knork';
 	(function() {
 		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
 		dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
