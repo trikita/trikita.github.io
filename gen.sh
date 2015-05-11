db3fbd668a7f61308aee206078a737cf16fbc512
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-05-11 23:53:12 +0300

    big cleanup

--- gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-KNORK_README=/home/serge/src/trikita/knork/README.md
-CHAIN_README=/home/serge/src/trikita/chain/README.md
-
-cd gen && npm install &&
-
-cd ../knork && node ../gen/build.js $KNORK_README index.tmpl &&
-cd ../chain && node ../gen/build.js $CHAIN_README index.tmpl
